ccf042642b880720005afd0f3d5fe52a4c73c667
htaylor91 <htaylor91@gmail.com>
2023-06-01 21:32:58 +0200

    feat(Modal): 💥 update Modal component structure
    Move responsibility of closing the modal to the showModal state set within parent, therefore update children(handleClose) to a standard children prop. Add animateClass prop to allow for fadeout animations. Make onClose prop optional. Add optional slot prop to be passed as slot attribute if present. Update comments with new examples.

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -2,79 +2,78 @@ import { useCallback, useEffect, useRef, ReactNode, CSSProperties } from 'react'
 
 export interface ModalProps {
   /**
-   * @important reset showModal state to false in the onClose event handler
-   * @example
-   * const onModalClose = () => {
-   *   //do something after the modal closes//
-   *   setShowModal(false) 
-   * }
-   * 
-   * @important pass the onModalClose handler to the onClose prop
-   * @example
-   * <Modal onClose={onModalClose} showModal={showModal}>
-   *    {modalChildren}
-   * </Modal>
-   * 
-   * @note This event bubbles in React.
-   */
-  onClose: (args?: any) => void
-  /**
    * @description
-   * The children prop is a function that returns the content of the modal.
+   * The modal component accepts a single child component.
+   * The elements in the child component can be in any order.
+   * However, at least one of the elements in the child component must be a button to close the modal.
    * 
-   * It receives the handleClose function as an argument,
+   * @important
+   * The button must have an onClick event handler that resets the showModal state to its initial state.
    * 
-   * which is used as the onClick event handler for the close button.
-   * @example
-   * const modalChildren = (handleClose: () => void) => {
-   *  return (
-   *   <>
-   *    <h2>Modal Title</h2>
-   *    <button onClick={handleClose} type="button">Close</button>
-   *   </>
-   *  )
-   * }
-   * 
-   * @important pass the modalChildren function to the children prop of the Modal component
-   * @example
-   *  <Modal onClose={onModalClose} showModal={showModal}>
-   *    {modalChildren}
-   *  </Modal>
-   * 
-   */
-  children: (handleClose: () => void) => ReactNode
-  /**
-   * @description
-   * The showModal prop is a boolean that controls whether the modal is visible or not.
-   * It is passed from the parent component.
    * @example
    * function App() {
    *   const [showModal, setShowModal] = useState<boolean>(false)
    * 
-   *   const onModalClose = () => {
+   *   const handleClose = () => {
    *     setShowModal(false)
    *   }
    * 
-   *   const modalChildren = (handleClose: () => void) => {
+   *   const ModalChildren = () => {
    *     return (
-   *      <>
+   *      <div className="modal-content">
    *        <h2>Modal Title</h2>
    *        <button onClick={handleClose} type="button">Close</button>
-   *      </>
-   *     )
+   *      </div>
+   *     );
    *   }
    *   
    *   return (
-   *    <div>
-   *      <button type='button' onClick={() => setShowModal(showModal => !showModal)}>Open Modal</button>
-   *      {<Modal onClose={onModalClose} showModal={showModal}>
-   *       {modalChildren}
-   *      </Modal>}
-   *    </div>
+   *     <div>
+   *       <button type='button' onClick={() => setShowModal(true}>Open Modal</button>
+   *       <Modal showModal={showModal}>
+   *        <ModalChildren/>
+   *       </Modal>
+   *     </div>
+   *   )
+   * }
+   */
+  children: ReactNode
+  /**
+   * @description
+   * The required showModal prop is a boolean that controls whether the modal is visible or not.
+   * It is passed from the parent component.
+   * 
+   * @important
+   * The onClick event handler for the close button should set the showModal state to its initial state.
+   * 
+   * @example
+   * function App() {
+   *   const [showModal, setShowModal] = useState<boolean>(false)
+   * 
+   *   const handleClose = () => {
+   *     setShowModal(false)
+   *   }
+   * 
+   *   const ModalChildren = () => {
+   *     return (
+   *      <div className="modal-content">
+   *        <h2>Modal Title</h2>
+   *        <button onClick={handleClose} type="button">Close</button>
+   *      </div>
+   *     );
+   *   }
+   *   
+   *   return (
+   *     <div>
+   *       <button type='button' onClick={() => setShowModal(true}>Open Modal</button>
+   *       <Modal showModal={showModal}>
+   *        <ModalChildren/>
+   *       </Modal>
+   *     </div>
    *   )
    * }
    * 
-   * @hint You can your own state management solution if you don't want to use React's built-in useState hook.
+   * @hint You can use your own state management solution if you don't want to use React's built-in useState hook.
    * @hint Inspect the dialog element in your browser's dev tools. Watch the open attribute change as you open and close the modal.
    */
   showModal: boolean
@@ -99,29 +98,45 @@ export interface ModalProps {
   returnValue?: string
   /**
    * @description
+   * The onClose prop accepts an event handler that is called when the user closes the modal.
+   * 
+   * @example
+   * const onModalClose = () => {
+   *   //do something after the modal closes//
+   *   console.log("The modal was closed")
+   * }
+   * 
+   * <Modal onClose={onModalClose} showModal={showModal}>
+   *    <ModalChildren/>
+   * </Modal>
+   * 
+   * @note This event bubbles in React.
+   */
+  onClose?: any
+  /**
+   * @description
    * The onCancel prop accepts an event handler that is called when the user dismisses the modal with the ESC (escape) key.
    * 
    * @example
-   *   //Required, fires after onModalCancel (if onModalCancel is present)
+   *   //Optional, fires after onModalCancel (if onModalCancel is present)
    *   const onModalClose = () => {
    *     console.log("The modal was closed")
-   *     setShowModal(false)
    *   }
    * 
-   *   //Optional, fires before onModalClose
+   *   //Optional, fires before onModalClose (if onModalClose is present)
    *   const onModalCancel = () => {
    *     console.log("The escape key was pressed")
    *   }
    * 
    * @important
    * The optional onCancel event handler 
-   * - is called before the required onClose event handler.
+   * - is called before the onClose event handler.
    * - is not called when the user clicks the close button.
    * - is not called when the user clicks outside the modal.
    * 
    * @note This event bubbles in React.
    */
-  onCancel?: (args?: any) => void
+  onCancel?: any
   /**
    * @hint You might not need a className prop.
    * @see https://developer.mozilla.org/en-US/docs/Web/CSS/:modal
@@ -171,14 +186,58 @@ export interface ModalProps {
    * @see https://react.dev/learn/rendering-lists#rules-of-keys
    */
   key?: string
+  /**
+   * @description
+   * The animateClass prop accepts a string that is used to add a CSS animation class to the dialog element.
+   *
+   * @example
+   * //Here the .close class is the animateClass prop value.
+   * <Modal animateClass="close" showModal={showModal}>
+   * 
+   * CSS
+   * @description
+   * - Fadout animation of the modal backdrop
+   * - The :backdrop CSS pseudo-class is used to style the modal backdrop
+   * 
+   * @example
+   * dialog.close::backdrop { animation: fadeout 1s forwards; }
+   * 
+   * @description
+   * - Fadeout animation of the dialog element itself (the modal)
+   * - The :modal CSS pseudo-class is used to style the dialog element while it is open (showing)
+   * 
+   * @example
+   * dialog.close:modal { animation: fadeout 1s forwards; }
+   *
+   * @description
+   * - The animateClass is not needed for a fadein animation
+   * - The fadeout animation is triggered by the animateClass prop value (in this case, "close")
+   * 
+   * @example
+   *  /@keyframes fadein {
+   *   from { opacity: 0; }
+   *   to { opacity: 1; }
+   *  }
+   * 
+   *  /@keyframes fadeout {
+   *   from { opacity: 1; }
+   *   to { opacity: 0; }
+   *  }
+   */
+  animateClass?: string
+  /**
+   * @see https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes/slot
+   * for more information about the slot attribute
+   */
+  slot?: string
 }
 
 const Modal = (
   {
-    onClose,
     children,
     showModal,
     returnValue,
+    onClose,
     onCancel,
     className,
     id,
@@ -190,27 +249,51 @@ const Modal = (
     translate,
     dir,
     key,
+    animateClass,
+    slot
   }: ModalProps) => {
 
   const dialogRef = useRef<HTMLDialogElement | null>(null);
 
-  useEffect(() => {
-    if (dialogRef.current && !dialogRef.current?.open && showModal) {
-      dialogRef.current.showModal();
-    }
-  }, [showModal]);
+  const closeModal = useCallback(() => {
+    dialogRef.current?.close(`${returnValue}`)
+  }, [returnValue]);
 
   const handleClose = useCallback(() => {
-    if (dialogRef.current?.open) {
-      dialogRef.current.close(`${returnValue}`);
+    if (!animateClass) {
+      closeModal()
+      return
     }
-  }, [returnValue]);
+
+    const closeAndCleanup = () => {
+      closeModal()
+      dialogRef.current?.classList.remove(`${animateClass}`);
+      dialogRef.current?.removeEventListener('animationend', closeAndCleanup);
+    }
+
+    dialogRef.current?.addEventListener('animationend', closeAndCleanup);
+    dialogRef.current?.classList.add(`${animateClass}`);
+
+  }, [animateClass, closeModal])
+
+  useEffect(() => {
+    if (!showModal) return
+
+    if (!dialogRef.current?.open) {
+      dialogRef.current?.showModal();
+    }
+
+    return () => {
+      handleClose()
+    }
+  }, [showModal, handleClose]);
+
 
   return (
     <dialog
       ref={dialogRef}
       onCancel={onCancel && onCancel}
-      onClose={onClose}
+      onClose={onClose && onClose}
       id={id && id}
       className={className && className}
       style={style && style}
@@ -221,8 +304,9 @@ const Modal = (
       translate={translate && translate}
       dir={dir && dir}
       key={key && key}
+      slot={slot && slot}
     >
-      {children(handleClose)}
+      {children}
     </dialog>
   );
 }
